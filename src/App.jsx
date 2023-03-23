590cd949c3b1f35671dd07a50b03f5b54bd3a005
Paulo Felipe <PauloFelipet15@gmail.com>
2023-03-23 09:30:54 -0300

    images configurarion model

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
-import images from './assets/exportImages'
+import tablet from '../src/assets/nav/tablet-portrait.svg'
+import people from '../src/assets/nav/people.svg'
+import cog from '../src/assets/nav/cog.svg'
+import document from '../src/assets/nav/document-text.svg'
+import filter from '../src/assets/filter.svg'
+import caneta from '../src/assets/Caneta.svg'
+import perfil from '../src/assets/perfil.png'
+import Vector from '../src/assets/Vector.svg'
+import Search from '../src/assets/Search.svg'
 import './App.css'
 
 function App() {
@@ -9,14 +17,14 @@ function App() {
     <div className="App">
       <div className="menu">
 
-        <img id='logoId' src={images.Vector} alt="" />
+        <img id='logoId' src={Vector} alt="" />
 
         <div className="menu-nav">
           <ul>
-            <li><button><img src={images.icons.tablet} alt="" />Boards</button>  </li>
-            <li><button><img src={images.icons.people} alt="" />Equipes</button></li>
-            <li><button><img src={images.icons.document}alt="" />Relatórios</button></li>
-            <li><button><img src={images.icons.cog}alt="" />Ajustes</button></li>
+            <li><button><img src={tablet} alt="" />Boards</button>  </li>
+            <li><button><img src={people} alt="" />Equipes</button></li>
+            <li><button><img src={document} alt="" />Relatórios</button></li>
+            <li><button><img src={cog} alt="" />Ajustes</button></li>
           </ul>
         </div>
 
@@ -26,26 +34,26 @@ function App() {
           <div className="main-header">
             <div className="title-kaban">
               <h1>Meu kaban</h1> <button>
-                <img src={images.caneta} alt="Icone caneta" />
+                <img src={caneta} alt="Icone caneta" />
               </button>
             </div>
-            <img src={images.perfil} alt="" />
+            <img src={perfil} alt="" />
           </div>
 
           <div className="main-filter">
             <button>
-              <img src={images.filter} alt="" />Filtrar
+              <img src={filter} alt="" />Filtrar
             </button>
             <div className="container-search">
-              <img src={images.Search} alt="" /> <input placeholder='Busque por cards, assuntos ou responsáveis...' type="search" />
+              <img src={Search} alt="" /> <input placeholder='Busque por cards, assuntos ou responsáveis...' type="search" />
 
             </div>
           </div>
         </div>
 
-    <div className="main-kanbans">
+        <div className="main-kanbans">
 
-    </div>
+        </div>
       </div>
     </div>
   )
