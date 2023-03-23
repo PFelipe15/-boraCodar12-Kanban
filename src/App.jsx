b776d1215b2b4e6dbd9f8d3b222be4f37943f41a
Paulo Felipe <PauloFelipet15@gmail.com>
2023-03-23 09:25:03 -0300

    images configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import images from './assets/exportImages'
 import './App.css'
 
 function App() {
@@ -9,14 +9,14 @@ function App() {
     <div className="App">
       <div className="menu">
 
-        <img id='logoId' src="../src/assets/Vector.svg" alt="" />
+        <img id='logoId' src={images.Vector} alt="" />
 
         <div className="menu-nav">
           <ul>
-            <li><button><img src="../src/assets/nav/tablet-portrait.svg" alt="" />Boards</button>  </li>
-            <li><button><img src="../src/assets/nav/people.svg" alt="" />Equipes</button></li>
-            <li><button><img src="../src/assets/nav/document-text.svg" alt="" />Relatórios</button></li>
-            <li><button><img src="../src/assets/nav/cog.svg" alt="" />Ajustes</button></li>
+            <li><button><img src={images.icons.tablet} alt="" />Boards</button>  </li>
+            <li><button><img src={images.icons.people} alt="" />Equipes</button></li>
+            <li><button><img src={images.icons.document}alt="" />Relatórios</button></li>
+            <li><button><img src={images.icons.cog}alt="" />Ajustes</button></li>
           </ul>
         </div>
 
@@ -26,24 +26,26 @@ function App() {
           <div className="main-header">
             <div className="title-kaban">
               <h1>Meu kaban</h1> <button>
-                <img src="../src/assets/Caneta.svg" alt="Icone caneta" />
+                <img src={images.caneta} alt="Icone caneta" />
               </button>
             </div>
-            <img src="../src/assets/perfil.png" alt="" />
+            <img src={images.perfil} alt="" />
           </div>
 
           <div className="main-filter">
             <button>
-              <img src="../src/assets/filter.svg" alt="" />Filtrar
+              <img src={images.filter} alt="" />Filtrar
             </button>
             <div className="container-search">
-              <img src="../src/assets/search.svg" alt="" /> <input placeholder='Busque por cards, assuntos ou responsáveis...' type="search" />
+              <img src={images.Search} alt="" /> <input placeholder='Busque por cards, assuntos ou responsáveis...' type="search" />
 
             </div>
           </div>
         </div>
 
+    <div className="main-kanbans">
 
+    </div>
       </div>
     </div>
   )
